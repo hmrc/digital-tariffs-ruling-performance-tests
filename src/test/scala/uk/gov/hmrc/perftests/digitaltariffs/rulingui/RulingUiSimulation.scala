bb1ff4a30e6d674be58e87e26955cac78292c372
Kuldeep Wadhwa <11062122+kuldeepwadhwa9@users.noreply.github.com>
2021-11-15 11:50:40 +0000

    DDCE-2472 upgraded performance test runner version (#6)

--- src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/rulingui/RulingUiSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/digitaltariffs/rulingui/RulingUiSimulation.scala
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.perftests.digitaltariffs.rulingui
 
-import io.gatling.core.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
 import uk.gov.hmrc.perftests.digitaltariffs.DigitalTariffsPerformanceTestRunner
@@ -11,11 +26,10 @@ class RulingUiSimulation extends PerformanceTestRunner with DigitalTariffsPerfor
   override val httpProtocol: HttpProtocolBuilder = {
     buildHttpProtocol(url = externalBaseUrl)
   }
-    setup("rulingUI", "Trader searches for rulings") withRequests(
-      getStartPage,
-      getQueryResultPage
+  setup("rulingUI", "Trader searches for rulings") withRequests(
+    getStartPage,
+    getQueryResultPage
   )
 
   runSimulation()
-
 }
